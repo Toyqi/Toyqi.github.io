b094e0592c0a34eae3b6080ee33e3c4b055ac6c4
Toyqi <wqibyr@gmail.com>
2013-09-09 18:29:46 +0800

    ci by Toyqi

---
+++ README.md
@@ -0,0 +1,6 @@
+toyqi.github.io
+==================
+
+我的个人博客：<http://toyqi.github.io>
+
+教程在这里：<http://www.lizherui.com/pages/2013/08/17/build_blog.html>
---
+++ pelicanconf.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Toyqi'
+SITENAME = u'Toyqi的程序世界'
+SITEURL = 'http://toyqi.github.io'
+
+
+GITHUB_URL = 'https://github.com/toyqi'
+ARCHIVES_URL = 'archives.html'
+ARTICLE_URL = 'pages/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+ARTICLE_SAVE_AS = 'pages/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+
+RELATIVE_URLS = True
+DEFAULT_PAGINATION = 5
+
+TIMEZONE = 'Asia/Shanghai'
+
+THEME = 'bootstrap2'
+
+DEFAULT_LANG = u'zh'
+
+DEFAULT_DATE_FORMAT = '%Y-%m-%d'
+
+GOOGLE_ANALYTICS = 'UA-43842699-1'
+
+GOOGLE_CUSTOM_SEARCH_SIDEBAR = '012191777864628038963:sjrtj5zxtec'
+
+DISQUS_SITENAME = 'wqiak47'
+
+FEED_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
+
+PLUGIN_PATH = u"pelican-plugins"
+PLUGINS = ["sitemap"]
+    
+## 配置sitemap 插件
+SITEMAP = {
+    "format": "xml",
+    "priorities": {
+        "articles": 0.7,
+        "indexes": 0.5,
+        "pages": 0.3,
+    },
+    "changefreqs": {
+        "articles": "monthly",
+        "indexes": "daily",
+        "pages": "monthly",
+    }
+}
+
+# Blogroll
+LINKS = (('Google', 'https://www.google.com/ncr'),
+          ('Python', 'http://python.org/'),
+          ('Pelican', 'http://docs.getpelican.com/'),
+         )
+
+# Social widget
+SOCIAL = (('Github', 'https://github.com/toyqi'),
+          (u'微博', 'http://weibo.com/wqibyr'),
+          (u'豆瓣', 'http://www.douban.com/people/Toyqi/'),
+         )
